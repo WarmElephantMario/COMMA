51ad67c38b46a2320dd1388926f43a28054b5b64
SiwonChoi25 <chlznf@naver.com>
2024-07-03 12:46:32 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,105 @@
+// import 'package:flutter/material.dart';
+
+// import '1_Splash_green.dart';
+
+// void main() {
+//   runApp(const FigmaToCodeApp());
+// }
+
+// class FigmaToCodeApp extends StatelessWidget {
+//   const FigmaToCodeApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData.dark().copyWith(
+//         scaffoldBackgroundColor: const Color.fromRGBO(54, 174, 146, 1.0),
+//       ),
+//       home: Scaffold(
+//         body: SplashScreen(),
+
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'folder_screen.dart';
+import 'start_learning_screen.dart';
+import 'mypage_screen.dart';
+import 'profile_screen.dart';
 
-import '1_Splash_green.dart';
+void main() => runApp(const MyApp());
 
-void main() {
-  runApp(const FigmaToCodeApp());
-}
-
-class FigmaToCodeApp extends StatelessWidget {
-  const FigmaToCodeApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color.fromRGBO(54, 174, 146, 1.0),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        body: SplashScreen(),
+      home: const MyHomePage(),
+    );
+  }
+}
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    FolderScreen(),
+    StartLearningScreen(),
+    MyPageScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder),
+            label: '폴더',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: '학습 시작',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '마이페이지',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green[900],
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        onTap: _onItemTapped,
       ),
     );
   }
