5bfb9b35837e129ccef411b803e1e5cd1a8a9b17
yena <aneykrap@gmail.com>
2024-07-03 12:03:22 +0900

    Add comma_script folder

---
+++ lib/colon.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'prepare.dart';
+
+class ColonPage extends StatefulWidget {
+  @override
+  _ColonPageState createState() => _ColonPageState();
+}
+
+class _ColonPageState extends State<ColonPage> {
+  int _currentIndex = 2; // 학습 시작 탭이 기본 선택되도록 설정
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0, // Hide the AppBar
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => LearningPreparation()),
+                    );
+                  },
+                  child: Text(
+                    '종료',
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/folder_search.png')),
+                const SizedBox(width: 8),
+                Text(
+                  '폴더 분류 > 기본 폴더',
+                  style: TextStyle(
+                    color: Color(0xFF575757),
+                    fontSize: 12,
+                    fontFamily: 'DM Sans',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '새로운 노트',
+              style: TextStyle(
+                color: Color(0xFF414141),
+                fontSize: 20,
+                fontFamily: 'DM Sans',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '강의 자료: Ch01. What is Algorithm?',
+              style: TextStyle(
+                color: Color(0xFF575757),
+                fontSize: 12,
+                fontFamily: 'DM Sans',
+              ),
+            ),
+            const SizedBox(height: 5), // 추가된 날짜와 시간을 위한 공간
+            Text(
+              '2024/06/07 오후 2:30',
+              style: TextStyle(
+                color: Color(0xFF575757),
+                fontSize: 12,
+                fontFamily: 'DM Sans',
+              ),
+            ),
+
+            const SizedBox(height: 20), // 강의 자료 밑에 여유 공간 추가
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: Text(
+                    '콜론(:) 다운하기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontFamily: 'DM Sans',
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0XFF36AE92),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Text(
+              '네 여러분 안녕하세요\n그래서 지난번에 공부한 Time Complexity 관련된 공식을 모두 공부해 오셨겠지요?\n다시 한 번 설명하지만 알고리즘에 있어서 Time complexity는 개발자라면 꼭 필수적으로 고려할 줄 알아야 하는 문제라고 했었음',
+              style: TextStyle(
+                color: Color(0xFF414141),
+                fontSize: 16,
+                fontFamily: 'DM Sans',
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/home.png')),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/folder.png')),
+            label: '폴더',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/learningstart.png')),
+            label: '학습 시작',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/mypage.png')),
+            label: '마이페이지',
+          ),
+        ],
+        selectedItemColor: Colors.teal,
+        unselectedItemColor: Colors.black,
+        selectedIconTheme: IconThemeData(color: Colors.teal),
+        unselectedIconTheme: IconThemeData(color: Colors.black),
+        selectedLabelStyle: TextStyle(
+          color: Colors.teal,
+          fontSize: 9, // 글씨 크기 설정
+          fontFamily: 'DM Sans', // 글씨체 설정
+          fontWeight: FontWeight.bold,
+        ), // 글씨 두께 설정),
+        unselectedLabelStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 9, // 글씨 크기 설정
+          fontFamily: 'DM Sans', // 글씨체 설정
+          fontWeight: FontWeight.bold,
+        ),
+        showUnselectedLabels: true, // 모든 텍스트 라벨을 항상 표시하도록 설정
+      ),
+    );
+  }
+}
---
+++ lib/prepare.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/material.dart';
+import 'popup_prepare.dart'; // Import the dialog file
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '오늘의 학습 준비하기',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: LearningPreparation(),
+    );
+  }
+}
+
+class LearningPreparation extends StatefulWidget {
+  @override
+  _LearningPreparationState createState() => _LearningPreparationState();
+}
+
+class _LearningPreparationState extends State<LearningPreparation> {
+  int _selectedOption = 0;
+  int _currentIndex = 2; // 학습 시작 탭이 기본 선택되도록 설정
+  bool _isMaterialEmbedded = false; // 강의 자료가 임베드되었는지 여부를 관리하는 변수
+  bool _isIconVisible = true; // 아이콘이 보이는지 여부를 관리하는 변수
+
+  void _showLearningDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return LearningDialog(this.context); // Pass the parent context
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0, // Hide the AppBar
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 15),
+            Text(
+              '오늘의 학습 준비하기',
+              style: TextStyle(
+                color: Color(0xFF414141),
+                fontSize: 24,
+                fontFamily: 'DM Sans',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              '학습 유형을 선택해주세요.',
+              style: TextStyle(
+                color: Color(0xFF575757),
+                fontSize: 16,
+                fontFamily: 'DM Sans',
+              ),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Radio(
+                  value: 0,
+                  groupValue: _selectedOption,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedOption = value!;
+                    });
+                  },
+                  activeColor: Color(0xFF36AE92),
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  '대체텍스트 생성',
+                  style: TextStyle(
+                    color: Color(0xFF414141),
+                    fontSize: 16,
+                    fontFamily: 'DM Sans',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: _selectedOption,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedOption = value!;
+                    });
+                  },
+                  activeColor: Color(0xFF36AE92),
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  '실시간 자막 생성',
+                  style: TextStyle(
+                    color: Color(0xFF414141),
+                    fontSize: 16,
+                    fontFamily: 'DM Sans',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    if (_isMaterialEmbedded) {
+                      _showLearningDialog(); // 버튼 클릭 시 팝업 창 표시
+                    } else {
+                      _isMaterialEmbedded = true; // 버튼 클릭 시 상태 업데이트
+                      _isIconVisible = false; // 아이콘 숨기기
+                    }
+                  });
+                },
+                icon: _isIconVisible
+                    ? ImageIcon(
+                  AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/Vector.png'),
+                  color: Colors.white,
+                )
+                    : Container(), // 아이콘이 보이지 않도록 빈 컨테이너 사용
+                label: Text(
+                  _isMaterialEmbedded ? '강의 자료 학습 시작하기' : '강의 자료를 임베드하세요',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontFamily: 'DM Sans',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0XFF36AE92),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            if (_isMaterialEmbedded) ...[
+              const SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.teal.shade50,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Icon(Icons.picture_as_pdf, color: Colors.red, size: 40),
+                    SizedBox(width: 15),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '인공지능_ch03_algorithm.pdf',
+                          style: TextStyle(
+                            color: Color(0xFF575757),
+                            fontSize: 15,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.w500,
+                            height: 1.2,
+                          ),
+                        ),
+                        Text(
+                          '2024년 6월 11일  845kb',
+                          style: TextStyle(
+                            color: Color(0xFF575757),
+                            fontSize: 14,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.w500,
+                            height: 1.2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/home.png')),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/folder.png')),
+            label: '폴더',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/learningstart.png')),
+            label: '학습 시작',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('/Users/bag-yena/StudioProjects/comma_script/lib/assets/navigation_bar/mypage.png')),
+            label: '마이페이지',
+          ),
+        ],
+        selectedItemColor: Colors.teal,
+        unselectedItemColor: Colors.black,
+        selectedIconTheme: IconThemeData(color: Colors.teal),
+        unselectedIconTheme: IconThemeData(color: Colors.black),
+        selectedLabelStyle: TextStyle(
+          color: Colors.teal,
+          fontSize: 9,
+          fontFamily: 'DM Sans',
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 9,
+          fontFamily: 'DM Sans',
+          fontWeight: FontWeight.bold,
+        ),
+        showUnselectedLabels: true,
+      ),
+    );
+  }
+}
