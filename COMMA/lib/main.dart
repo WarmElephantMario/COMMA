a83eb2ba41490838163a3f8c664314646211232b
tunaunnie <ygw021227@gmail.com>
2024-07-04 17:29:09 +0900

    signin

--- COMMA/lib/main.dart
+++ COMMA/lib/main.dart
@@ -93,3 +93,30 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import '1_Splash_green.dart';
+// import '5_Signup.dart';
+//
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(const FigmaToCodeApp());
+// }
+//
+// class FigmaToCodeApp extends StatelessWidget {
+//   const FigmaToCodeApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       theme: ThemeData.dark().copyWith(
+//         scaffoldBackgroundColor: const Color.fromRGBO(54, 174, 146, 1.0),
+//       ),
+//       home: Scaffold(
+//         body: SplashScreen(),
+//       ),
+//     );
+//   }
+// }
+//
