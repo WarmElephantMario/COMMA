290d6163a3e3fd5ea3f0d1f1af319f3781df5fa0
tunaunnie <ygw021227@gmail.com>
2024-07-05 01:09:33 +0900

    lecture_folder

--- COMMA/lib/api/api.dart
+++ COMMA/lib/api/api.dart
@@ -1,9 +1,14 @@
 class API{
-  static const hostConnect = "http://172.30.104.81/api_new_members";
+  static const hostConnect = "http://192.168.219.155/api_new_members";
+  static const hostConnect2 = "http://192.168.219.155/api_new_lectures";
 
   static const hostConnectUser = "$hostConnect/user";
+  static const hostConnectLectureFile = "$hostConnect2/lecture_folder";
 
   static const signup = "$hostConnectUser/signup.php";
   static const validatePhone = "$hostConnectUser/validate_phone.php";
   static const login = "$hostConnectUser/login.php";
+
+  static const getAllFolders = "$hostConnectLectureFile/getAllFolders.php";
+  static const deleteFolder = "$hostConnectLectureFile/deleteFolder.php";
 }
