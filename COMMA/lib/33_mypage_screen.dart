f2f74cb110bb3db60c59770a000577ddc6ceff5f
SiwonChoi25 <chlznf@naver.com>
2024-07-12 14:49:53 +0900

    logout, leave modify

--- COMMA/lib/33_mypage_screen.dart
+++ COMMA/lib/33_mypage_screen.dart
@@ -68,10 +68,9 @@ class _MyPageScreenState extends State<MyPageScreen> {
         Fluttertoast.showToast(msg: '회원 탈퇴가 완료되었습니다.');
         // SplashGreenScreen 화면으로 이동
         Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => const SplashScreen()),
-          (Route<dynamic> route) => false,
-        );
+            context,
+            MaterialPageRoute(builder: (context) => const SplashScreen()),
+            (Route<dynamic> route) => false);
       } else {
         Fluttertoast.showToast(msg: '회원 탈퇴 중 오류가 발생했습니다.');
       }
