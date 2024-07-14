39aad98a558eaa0242f47dd6a91fc9727d457f33
tunaunnie <ygw021227@gmail.com>
2024-07-14 13:21:04 +0900

    gpt-4o connection

---
+++ COMMA/lib/env/env.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const String apiKey =
+      'sk-proj-EECg9ziQH6Jfow6cMu0iT3BlbkFJs3NgGaSOeJzdllNI05D9';
+}
