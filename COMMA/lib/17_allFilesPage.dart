25316823fd9351e4d08cb1b516a6d5a2e61859b7
tunaunnie <ygw021227@gmail.com>
2024-07-15 22:47:32 +0900

    alt_text connect

--- COMMA/lib/17_allFilesPage.dart
+++ COMMA/lib/17_allFilesPage.dart
@@ -83,6 +83,8 @@ class _AllFilesPageState extends State<AllFilesPage> {
             folderName: folderName,
             recordingState: RecordingState.recorded,
             lectureName: file['lecture_name'] ?? 'Unknown Lecture',
+            responseUrl: 'https://defaulturl.com/defaultfile.txt', //수정 필요
+            type: 1, //수정 필요
           )
         : ColonPage(
             folderName: folderName,
