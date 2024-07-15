15c8cac7b65e523a33c503ce45f0cd0d8467b61c
tunaunnie <ygw021227@gmail.com>
2024-07-15 22:47:32 +0900

    alt_text connect

--- COMMA/lib/62lecture_start.dart
+++ COMMA/lib/62lecture_start.dart
@@ -10,12 +10,16 @@ import 'api/api.dart';
 class LectureStartPage extends StatefulWidget {
   final String fileName;
   final String fileURL;
+  final String responseUrl;
+  final int type;
 
 
   const LectureStartPage({
     Key? key,
     required this.fileName,
     required this.fileURL,
+    required this.responseUrl,
+    required this.type,
   }) : super(key: key);
 
   @override
@@ -449,6 +453,8 @@ body: SingleChildScrollView(
                   folderName: _selectedFolder,
                   recordingState: RecordingState.initial,
                   lectureName: widget.fileName,
+                  responseUrl: widget.responseUrl,
+                  type: widget.type,
                 ),
               ),
             );
--- COMMA/macos/Flutter/GeneratedPluginRegistrant.swift
+++ COMMA/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import device_info_plus
 import file_selector_macos
 import firebase_core
 import firebase_storage
+import path_provider_foundation
 import pdf_render
 import pdfx
 import shared_preferences_foundation
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
