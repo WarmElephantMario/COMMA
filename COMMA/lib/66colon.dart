0c540ebe4690ca65f5513493b5a7c060a9be605a
yena <aneykrap@gmail.com>
2024-07-19 11:39:23 +0900

    colon img + text

--- COMMA/lib/66colon.dart
+++ COMMA/lib/66colon.dart
@@ -95,96 +95,98 @@ class _ColonPageState extends State<ColonPage> {
       ),
       body: isLoading
           ? Center(child: CircularProgressIndicator())
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const LearningPreparation()),
-                              );
-                            },
-                            child: const Text(
-                              '종료',
-                              style: TextStyle(
-                                color: Color(0xFFFFA17A),
-                                fontSize: 16,
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const LearningPreparation()),
+                                );
+                              },
+                              child: const Text(
+                                '종료',
+                                style: TextStyle(
+                                  color: Color(0xFFFFA17A),
+                                  fontSize: 16,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Image.asset('assets/folder_search.png'),
-                          const SizedBox(width: 8),
-                          Text(
-                            '폴더 분류 > ${widget.folderName}', // 폴더 이름 사용
-                            style: const TextStyle(
-                              color: Color(0xFF575757),
-                              fontSize: 12,
-                              fontFamily: 'DM Sans',
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Image.asset('assets/folder_search.png'),
+                            const SizedBox(width: 8),
+                            Text(
+                              '폴더 분류 > ${widget.folderName}', // 폴더 이름 사용
+                              style: const TextStyle(
+                                color: Color(0xFF575757),
+                                fontSize: 12,
+                                fontFamily: 'DM Sans',
+                              ),
                             ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          widget.noteName, // 노트 이름 사용
+                          style: const TextStyle(
+                            color: Color(0xFF414141),
+                            fontSize: 20,
+                            fontFamily: 'DM Sans',
+                            fontWeight: FontWeight.bold,
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        widget.noteName, // 노트 이름 사용
-                        style: const TextStyle(
-                          color: Color(0xFF414141),
-                          fontSize: 20,
-                          fontFamily: 'DM Sans',
-                          fontWeight: FontWeight.bold,
                         ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        '강의 자료 : ${widget.lectureName}',
-                        style: const TextStyle(
-                          color: Color(0xFF575757),
-                          fontSize: 12,
-                          fontFamily: 'DM Sans',
-                        ),
-                      ),
-                      const SizedBox(height: 5), // 추가된 날짜와 시간을 위한 공간
-                      Text(
-                        _formatDate(widget.createdAt), // 데이터베이스에서 가져온 생성 날짜 및 시간 사용
-                        style: const TextStyle(
-                          color: Color(0xFF575757),
-                          fontSize: 12,
-                          fontFamily: 'DM Sans',
-                        ),
-                      ),
-                      const SizedBox(height: 20), // 강의 자료 밑에 여유 공간 추가
-                      Row(
-                        children: [
-                          ClickButton(
-                            text: '콜론(:) 다운하기',
-                            onPressed: () {},
-                            width: MediaQuery.of(context).size.width * 0.3,
-                            height: 40.0,
+                        const SizedBox(height: 5),
+                        Text(
+                          '강의 자료 : ${widget.lectureName}',
+                          style: const TextStyle(
+                            color: Color(0xFF575757),
+                            fontSize: 12,
+                            fontFamily: 'DM Sans',
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                    ],
+                        ),
+                        const SizedBox(height: 5), // 추가된 날짜와 시간을 위한 공간
+                        Text(
+                          _formatDate(widget.createdAt), // 데이터베이스에서 가져온 생성 날짜 및 시간 사용
+                          style: const TextStyle(
+                            color: Color(0xFF575757),
+                            fontSize: 12,
+                            fontFamily: 'DM Sans',
+                          ),
+                        ),
+                        const SizedBox(height: 20), // 강의 자료 밑에 여유 공간 추가
+                        Row(
+                          children: [
+                            ClickButton(
+                              text: '콜론(:) 다운하기',
+                              onPressed: () {},
+                              width: MediaQuery.of(context).size.width * 0.3,
+                              height: 40.0,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
                   ),
-                ),
-                if (widget.lectureName.endsWith('.pdf') && widget.fileUrl != null)
-                  Expanded(
-                    child: CustomScrollView(
+                  if (widget.lectureName.endsWith('.pdf') &&
+                      widget.fileUrl != null)
+                    CustomScrollView(
+                      shrinkWrap: true,
                       slivers: [
                         SliverList(
                           delegate: SliverChildBuilderDelegate(
@@ -223,20 +225,32 @@ class _ColonPageState extends State<ColonPage> {
                         ),
                       ],
                     ),
-                  ),
-                if ((widget.lectureName.endsWith('.png') ||
-                        widget.lectureName.endsWith('.jpg') ||
-                        widget.lectureName.endsWith('.jpeg')) &&
-                    imageData != null)
-                  Expanded(
-                    child: Image.memory(
-                      imageData!,
-                      fit: BoxFit.cover, // 이미지를 전체 화면에 맞춤
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height - 200, // 화면 높이에 맞춤
+                  if ((widget.lectureName.endsWith('.png') ||
+                      widget.lectureName.endsWith('.jpg') ||
+                      widget.lectureName.endsWith('.jpeg')) &&
+                      imageData != null)
+                    Column(
+                      children: [
+                        Image.memory(
+                          imageData!,
+                          fit: BoxFit.cover, // 이미지를 전체 화면에 맞춤
+                          width: double.infinity,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            pageTexts[1] ?? '이미지의 텍스트가 없습니다.',
+                            style: const TextStyle(
+                              color: Color(0xFF414141),
+                              fontSize: 16,
+                              fontFamily: 'DM Sans',
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-              ],
+                ],
+              ),
             ),
       bottomNavigationBar:
           buildBottomNavigationBar(context, _selectedIndex, _onItemTapped),
