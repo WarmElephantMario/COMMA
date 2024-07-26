6e583d6898382d1bc16fb186e0f16f46f6c3b352
tunaunnie <ygw021227@gmail.com>
2024-07-26 16:25:17 +0900

    please

--- COMMA/lib/api/api.dart
+++ COMMA/lib/api/api.dart
@@ -2,9 +2,10 @@ class API {
   static String get baseUrl {
     const env = String.fromEnvironment('ENV', defaultValue: 'development');
     if (env == 'production') {
-      return 'https://your-production-url.com'; // 서버 URL
+      return 'http://ec2-3-144-116-32.us-east-2.compute.amazonaws.com:3000'; // 서버 URL
     } else {
-      return 'http://10.240.222.239:3000'; // 개발 환경 URL
+      return 'http://10.240.218.120:3000'; // 개발 환경 URL
     }
   }
 }
+
