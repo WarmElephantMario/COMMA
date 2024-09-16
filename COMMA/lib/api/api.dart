1171180587010bf68427df502328d1815d74b4a6
tunaunnie <ygw021227@gmail.com>
2024-09-16 11:58:45 +0900

    add switch

--- COMMA/lib/api/api.dart
+++ COMMA/lib/api/api.dart
@@ -1,9 +1,9 @@
-// class API {
-//   static String get baseUrl {
-//     return 'http://ec2-52-79-168-67.ap-northeast-2.compute.amazonaws.com:3000';
-//   }
-// }
-
 class API {
-  static const baseUrl = 'http://192.168.219.169:3000';
+  static String get baseUrl {
+    return 'http://ec2-52-79-168-67.ap-northeast-2.compute.amazonaws.com:3000';
+  }
 }
+
+// class API {
+//   static const baseUrl = 'http://192.168.219.169:3000';
+// }
