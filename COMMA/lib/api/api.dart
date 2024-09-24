0231bca81e40ca6917283d28bd2949594e3d6910
yena <aneykrap@gmail.com>
2024-09-24 12:44:47 +0900

    save

--- COMMA/lib/api/api.dart
+++ COMMA/lib/api/api.dart
@@ -1,13 +1,13 @@
-// class API {
-//   static String get baseUrl {
-//     return 'http://ec2-52-79-168-67.ap-northeast-2.compute.amazonaws.com:3000';
-//   }
-
-
-// }
-
 class API {
+  static String get baseUrl {
+    return 'http://ec2-52-79-168-67.ap-northeast-2.compute.amazonaws.com:3000';
+  }
 
-  static const baseUrl = 'http://192.168.45.179:3000';
 
 }
+
+// class API {
+
+//   static const baseUrl = 'http://192.168.45.179:3000';
+
+// }
