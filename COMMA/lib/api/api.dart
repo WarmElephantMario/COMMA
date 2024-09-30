767eeb78f7989cb0eeb13e7878fa6a2a3c3eba52
tunaunnie <ygw021227@gmail.com>
2024-09-30 22:54:31 +0900

    api address modify

--- COMMA/lib/api/api.dart
+++ COMMA/lib/api/api.dart
@@ -1,11 +1,11 @@
-// class API {
-//   static String get baseUrl {
-//     return 'http://ec2-52-79-168-67.ap-northeast-2.compute.amazonaws.com:3000';
-//   }
-
-// }
-
 class API {
-  static const baseUrl = 'http://172.20.10.12:3000';
+  static String get baseUrl {
+    return 'http://ec2-52-79-168-67.ap-northeast-2.compute.amazonaws.com:3000';
+  }
 
 }
+
+// class API {
+//   static const baseUrl = 'http://172.20.10.12:3000';
+
+// }
